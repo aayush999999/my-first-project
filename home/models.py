4883a7d5a87d6f1fda1c378513505b25b707e1d0
aayush999999 <aayush.sri1@gmail.com>
2023-11-02 22:51:39 +0530

    added validatin

--- home/models.py
+++ home/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Registration(models.Model):
-    name=models.CharField(max_length=122)
-    password=models.CharField(max_length=122)
+    name=models.CharField(max_length=30)
+    password=models.CharField(max_length=30)
+   
     date=models.DateField()
 
     def __str__(self):
         return self.name
+    
--- home/views.py
+++ home/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, HttpResponse
 from datetime import datetime
 from home.models import Registration
+# from django.contrib.auth.models import Registration
 from django.contrib import messages
+from django.core.exceptions import ValidationError
+
 
 # Create your views here.
 def index(request):
@@ -21,19 +24,45 @@ def contact(request):
     #return HttpResponse("this is contact page Mobile No. :- 6394983436")
 
 def reg(request):
-    context = {}
+    # context = {}
     if request.method == "POST":
-        print(request.POST)
-        name=request.POST.get('name')
-        password=request.POST.get('password')
-        contact=Registration(name=name,password=password,date=datetime.today())
-        messages.success(request,"Successfully Registered")
-        contact.save()
-        context["name"] = name
+        
 
-    return render(request, 'reg.html', context=context)
+        # print(request.POST)
+        name = request.POST.get('name')
+        password = request.POST.get('password')
+        if not name :
+            messages.success(request,"name is blank")
+            return render(request, 'reg.html', ) 
+        
+        if not password :
+            messages.success(request,"password is blank")
+            return render(request, 'reg.html', ) 
+        
+        if len(password) < 6:
+            messages.success(request, 'Password too short')
+            return render(request, 'reg.html', )
+
+        # myuser = Registration.objects.create_registration(name, password)
+        # myuser.save()
+        # myuser=Registration(name=name,password=password)
+        # contact = Registration(name=name,password=password,date=datetime.today())
+        # contact.save()
+
+        Registration.objects.create(name=name,password=password,date=datetime.today())
+
+        # context["name"] = name
+
+        messages.success(request,"Successfully Registered")
+        
+
+    return render(request, 'reg.html', ) #context=context
     #return HttpResponse("this is services page")
 
 
 def login(request):
     return render(request, 'login.html')
+
+ 
+# def logout(request):
+#     pass
