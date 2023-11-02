aeeb1129c356bbbf8dc60268731c8ca2f8fd0127
aayush999999 <aayush.sri1@gmail.com>
2023-11-02 16:34:43 +0530

    formsubmission

---
+++ templates/javascript.js
@@ -0,0 +1,10 @@
+{% comment %} function func(){
+    event.preventDefault();
+
+    console.log('not refreshed!!!!')
+} {% endcomment %}
+
+
+formSubmit.addEventListener('click', function(event){
+    event.preventDefault();
+});
