25a7cd5c7ff5484da341b3b32e7fd1db8967ff64
aayush999999 <aayush.sri1@gmail.com>
2023-12-23 08:46:37 +0530

    cretae blogpost model

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from home.models import Registration,ItemInsert,Contact,Checkout,OrderUpdate
+from home.models import Registration,ItemInsert,Contact,Checkout,OrderUpdate,Blogpost
 
 
 # Register your models here.
@@ -8,3 +8,4 @@ admin.site.register(ItemInsert)
 admin.site.register(Contact)
 admin.site.register(Checkout)
 admin.site.register(OrderUpdate)
+admin.site.register(Blogpost)
--- home/models.py
+++ home/models.py
@@ -63,3 +63,19 @@ class OrderUpdate(models.Model):
 
     def __str__(self):
         return self.update_desc[0:7] + "..."
+    
+
+class Blogpost(models.Model):
+    post_id = models.AutoField(primary_key= True)
+    tilte = models.CharField(max_length=50)
+    head0 = models.CharField(max_length=500, default="")
+    chead0 = models.CharField(max_length=5000, default="")
+    head1 = models.CharField(max_length=500, default="")
+    chead1 = models.CharField(max_length=5000, default="")
+    head2 = models.CharField(max_length=500, default="")
+    chead2 = models.CharField(max_length=5000, default="")
+    pub_date = models.DateField()
+    thumbnail = models.ImageField(upload_to='seller/images', default="")
+
+    def __str__(self):
+        return self.tilte
